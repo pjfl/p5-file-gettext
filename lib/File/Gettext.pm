d1795f82ac47074f64ada3a78eaa75baa0c430a3
pjfl <github@roxsoft.co.uk>
2015-10-01 16:23:06 +0100

    Switched Travis to a containerised build

--- lib/File/Gettext.pm
+++ lib/File/Gettext.pm
@@ -2,7 +2,7 @@ package File::Gettext;
 
 use 5.010001;
 use namespace::autoclean;
-use version; our $VERSION = qv( sprintf '0.29.%d', q$Rev: 4 $ =~ /\d+/gmx );
+use version; our $VERSION = qv( sprintf '0.29.%d', q$Rev: 5 $ =~ /\d+/gmx );
 
 use English                    qw( -no_match_vars );
 use File::DataClass::Constants qw( EXCEPTION_CLASS FALSE NUL SPC TRUE );
@@ -194,7 +194,7 @@ File::Gettext - Read and write GNU Gettext po / mo files
 
 =head1 Version
 
-This documents version v0.29.$Rev: 4 $ of L<File::Gettext>
+This documents version v0.29.$Rev: 5 $ of L<File::Gettext>
 
 =head1 Synopsis
 
